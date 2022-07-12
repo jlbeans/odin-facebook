69a04702da1c607aadbcf22ac9d2c957506228be
jlbeans <jm@cselmer.com>
2022-07-12 16:32:27 -0700

    set up devise

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "users#index"
+end
